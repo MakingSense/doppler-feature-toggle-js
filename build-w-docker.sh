76411c4f9c6f1c6713112c9b94fc066d19cc660a
Andrés Moschini <amoschini@makingsense.com>
2018-08-19 17:52:04 -0300

    ERROR!

--- build-w-docker.sh
+++ build-w-docker.sh
@@ -8,7 +8,7 @@ set -e
 # Lines added to get the script running in the script path shell context
 # reference: http://www.ostricher.com/2014/10/the-right-way-to-get-the-directory-of-a-bash-script/
 cd $(dirname $0)
-
+ERROR!!
 docker run --rm \
     -v /`pwd`:/work \
     -w /work \
